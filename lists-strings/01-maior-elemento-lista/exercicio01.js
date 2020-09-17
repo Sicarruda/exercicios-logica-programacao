40f15d1b265ded4b57a395a73336cede44d6d0bb
Jessica Arruda <sicarruda@gmail.com>
2020-09-17 15:21:59 -0300

    correção da função

--- lists-strings/01-maior-elemento-lista/exercicio01.js
+++ lists-strings/01-maior-elemento-lista/exercicio01.js
@@ -2,28 +2,22 @@
 //Write a function that returns the largest element in a list.
 
 var lista = [1,23,4,35,6,7,89,10,58,20,78,59,60,80];
-var maiorLista = 0
-function retornarMaior(){
-    for(var i = 0; i <= lista.length; i++){
-        if (lista[i] > maiorLista){
-            maiorLista = lista[i];
-        }
+
+function retornarMaior(array){
+    var maiorElemento = array[0];
+    if(array.length > 0){
+        for(var i = 0; i < array.length; i++){
+            if (array[i] > maiorElemento){
+                maiorElemento = array[i];
+            }   
+        }return maiorElemento;
+    }else {
+        return null;
     }
-    console.log(maiorLista);
 }
-retornarMaior();
+console.log(retornarMaior(lista));
 
-
-var listaNegativos = [-23,-4,-35,-6,-7,-89,-10,-58,-20,-78,-59,-60,-1,-80];
-
-var maiorListaNegativo = listaNegativos[0];
-
-function retornarMaiorNegativo(){
-    for(var i = 0; i <= listaNegativos.length; i++){
-        if (listaNegativos[i] > maiorListaNegativo){
-            maiorListaNegativo = listaNegativos[i];
-        }
-    }
-    console.log(maiorListaNegativo);
-}
-retornarMaiorNegativo();
+var listaNegativos = [-23,-4,-35,-6,-7,-89,-10,-58,-20,-78,-59,-60,-80];
+var listaVazia = [];
+console.log(retornarMaior(listaNegativos));
+console.log(retornarMaior(listaVazia));
