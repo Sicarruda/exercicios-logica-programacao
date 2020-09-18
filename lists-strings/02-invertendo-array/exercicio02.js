3880dfc6077fb372335c8cc99dad670d60b28a26
Jessica Arruda <sicarruda@gmail.com>
2020-09-18 14:35:56 -0300

    Correção no nome de variavel

--- lists-strings/02-invertendo-array/exercicio02.js
+++ lists-strings/02-invertendo-array/exercicio02.js
@@ -4,13 +4,13 @@ var lista = [120,655,26,85,323,65,778,12,10,5];
 
 
 function inverterLista(array){
-    var elemento = 0;
+    var index = 0;
     var listaFinal = [];
     var a = 0;
     for(var i = 0; i < array.length; i++){    
         a = a + 1;
-        elemento = array.length - a;
-        var ultimoElemento = array[elemento];
+        index = array.length - a;
+        var ultimoElemento = array[index];
         listaFinal.push(ultimoElemento);
     }
     return listaFinal;
