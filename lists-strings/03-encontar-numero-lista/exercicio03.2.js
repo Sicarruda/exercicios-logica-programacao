6d19c22507b6838c4188028ab23de0dea5be2deb
Jessica Arruda <sicarruda@gmail.com>
2020-09-21 15:13:33 -0300

    Correção de identação.

--- lists-strings/03-encontar-numero-lista/exercicio03.2.js
+++ lists-strings/03-encontar-numero-lista/exercicio03.2.js
@@ -4,10 +4,10 @@ var usuarioInput = prompt("Qual o numero você quer?");
 var lista = [0,6,12,36,98,1,12,50,98,12,36,12];
 
 function compararNumero(numeroUsuario,array){
-    var controle = 0
+    var controle = 0;
     for(var i = 0; i < array.length; i++){
         if(numeroUsuario == array[i]){
-         controle = controle + 1;               
+            controle = controle + 1;               
         }
     }
     return controle;
