b7b22c528b8f1572b5346ad2761ab54b021a50c7
Jessica Arruda <sicarruda@gmail.com>
2020-09-30 15:14:52 -0300

    Simplificação da função

--- 12-fibonacci/exercicio12.js
+++ 12-fibonacci/exercicio12.js
@@ -1,18 +1,9 @@
 // f(n)=f(n-1)+f(n-2)
 
 function fibonacci(n){
-    var conta = 0;
-    var conta1 = 0;
-    var conta2 = 0;
-    if(n == 0){
-        var conta2 = 0;
-    }else if(n == 1){
-        conta1 = 1;
-    }else{
-        conta1 = fibonacci(n-1);
-        conta2 = fibonacci(n-2);
-    }
-    return conta = conta1 + conta2;
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 
-console.log(fibonacci(10));
+console.log(fibonacci(20));
